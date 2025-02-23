ad181a3760641baf5627e64852af366a63fd9cb9
Siva010 <ms.ivas010@gmail.com>
2025-02-23 17:10:55 +0530

    changed

---
+++ app/dashboard/page.js
@@ -0,0 +1,12 @@
+"use client";
+import React, { useEffect } from "react";
+import { useTheme } from "next-themes";
+function Dashboard() {
+  const { setTheme } = useTheme();
+  useEffect(() => {
+    setTheme("light");
+  }, []);
+  return <div>page</div>;
+}
+
+export default Dashboard;
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "./ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,6 +23,12 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        ></ThemeProvider>
         {children}
       </body>
     </html>
