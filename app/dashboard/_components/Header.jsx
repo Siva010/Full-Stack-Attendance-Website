2b1e6689bc35e97bbffc317cbd054bed00a7a766
Siva010 <ms.ivas010@gmail.com>
2025-02-24 22:20:16 +0530

    frontendpartial

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,7 +1,25 @@
+"use client";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import Image from "next/image";
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  const { user } = useKindeBrowserClient();
+  return (
+    <div className="p-4 shadow-lg border flex justify-between">
+      <div></div>
+
+      <div>
+        <Image
+          src={user?.picture}
+          width={35}
+          height={35}
+          alt="user"
+          className="rounded-full"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
